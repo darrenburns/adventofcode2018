e08e8c401c45687661ee682816b9379e99c8e31d
Darren Burns <darren.burns@fanduel.com>
2018-12-05 20:17:42 +0000

    cleanup

--- src/one.rs
+++ src/one.rs
@@ -4,8 +4,6 @@ use std::io::{BufRead, BufReader, Result};
 pub fn calculate_frequency() -> Result<i32> {
     let file = File::open("src/files/one/frequencies.txt")?;
     BufReader::new(file).lines()
-        .fold(Ok(0), |acc, line| {
-            Ok(acc? + line?.parse::<i32>().unwrap())
-        })
+        .fold(Ok(0), |acc, line| Ok(acc? + line?.parse::<i32>().unwrap()))
 }
 
