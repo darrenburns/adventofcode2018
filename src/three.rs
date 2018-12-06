3bedae3171423ac84f8c592dd345ea5e75d5fdb2
Darren Burns <darren.burns@fanduel.com>
2018-12-06 02:20:46 +0000

    day 3 part 2

--- src/three.rs
+++ src/three.rs
@@ -4,36 +4,68 @@ use std::io::BufReader;
 
 type Tiles = Vec<Vec<i32>>;
 
+#[derive(Debug)]
 struct Location {
     row: u16,
     col: u16,
 }
 
+#[derive(Debug)]
 struct Dimension {
     height: u16,
     width: u16,
 }
 
+#[derive(Debug)]
 struct Claim {
     location: Location,
     dimensions: Dimension,
 }
 
-// Time: 0.20s
+// Time: 0.20s (for part 1 only)
+// Time: 0.23s (for part 1 + part 2)
 pub fn find_num_square_inches_overlapping() -> usize {
     // How many square inches of tiles are within two or more claims
     // i.e. how many elements of the matrix have a value > 1
     let mut tiles = vec![vec![0; 1024]; 1024];
+    let mut claims = Vec::with_capacity(1347);
 
     let file = File::open("src/files/three/claims.txt").unwrap();
     let lines = BufReader::new(file).lines().map(|l| l.unwrap());
 
     for line in lines {
         let claim = deserialise_claim(&line);
-        add_layer_to_tiles(&mut tiles, claim);
+        add_layer_to_tiles(&mut tiles, &claim);
+        claims.push(claim);
     }
 
-    count_overlaps(tiles)
+    let num_overlaps = count_overlaps(&tiles);
+
+    // Part 2 - find the single claim that overlaps with nothing.
+    for claim in &claims {
+        let Claim { location, dimensions } = claim;
+        let Location { row, col } = location;
+        let Dimension { height, width } = dimensions;
+
+        let mut has_overlaps = false;
+        for row_idx in *row..row + height {
+            let row = tiles.get_mut(row_idx as usize).unwrap();
+            for col_idx in *col..col + width {
+                if row[col_idx as usize] != 1 {
+                    has_overlaps = true;
+                }
+            }
+        }
+
+        // If we get here without breaking out of 'claims, all tiles were 0 and this
+        // claim hasn't overlapped with anything, so print it.
+        if !has_overlaps {
+            println!("Non-overlapping claim: {:?}", claim);
+            break;
+        }
+    }
+
+    num_overlaps
 }
 
 // #1 @ 1,3: 4x4
@@ -57,20 +89,20 @@ fn deserialise_claim(line: &str) -> Claim {
     Claim { location, dimensions }
 }
 
-fn add_layer_to_tiles(tiles: &mut Tiles, claim: Claim) {
+fn add_layer_to_tiles(tiles: &mut Tiles, claim: &Claim) {
     let Claim { location, dimensions } = claim;
     let Location { row, col } = location;
     let Dimension { height, width } = dimensions;
 
-    for row_idx in row..(row + height) {
+    for row_idx in *row..(*row + *height) {
         let row = tiles.get_mut(row_idx as usize).unwrap();
 
-        for col_idx in col..(col + width) {
-             row[col_idx as usize] += 1;
+        for col_idx in *col..(*col + *width) {
+            row[col_idx as usize] += 1;
         }
     }
 }
 
-fn count_overlaps(tiles: Tiles) -> usize {
+fn count_overlaps(tiles: &Tiles) -> usize {
     tiles.iter().flatten().filter(|t| **t > 1).count()
 }
