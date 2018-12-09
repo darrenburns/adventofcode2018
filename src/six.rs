0271fdb5483b64b2008e87d468ed0d36887f4acb
Darren Burns <darren.burns@fanduel.com>
2018-12-09 02:55:34 +0000

    rewrite day 6 part 1 using variant types

--- src/six.rs
+++ src/six.rs
@@ -8,145 +8,169 @@ use std::usize;
 
 use uuid::Uuid;
 
-const COORDS_FILE: &'static str = "src/files/six/example.txt";
+const COORDS_FILE: &'static str = "src/files/six/coords.txt";
 
-type Grid = Vec<Vec<Point>>;
+type Grid = Vec<Vec<GridCell>>;
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
-struct Point { id: String, x: usize, y: usize, closest_id: Option<String> }
+enum GridCell {
+    // We ignore GridCells when they're tied closest to 2 points
+    Border,
+    // A point from the input list
+    Point { id: String, row: usize, col: usize },
+    // A cell which wasn't part of the input list, but belongs within the territory one of those points
+    TerritoryOfPoint { point_id: String },
+    // A GridCell that hasn't been initialised
+    Empty,
+}
 
-impl Point {
-    fn new() -> Self {
-        Point { id: Uuid::new_v4().to_string(), x: 0, y: 0, closest_id: None }
+impl GridCell {
+    fn point(row: usize, col: usize) -> Self {
+        GridCell::Point { id: Uuid::new_v4().to_string(), row, col }
     }
-    fn with_owner(closest_id: String) -> Self {
-        Point { id: Uuid::new_v4().to_string(), x: 0, y: 0, closest_id: Some(closest_id) }
+    fn in_territory_of(point_id: String) -> Self {
+        GridCell::TerritoryOfPoint { point_id }
     }
-    fn self_owned(x: usize, y: usize) -> Self {
-        let uuid = Uuid::new_v4().to_string();
-        Point { id: uuid.clone(), x, y, closest_id: Some(uuid) }
+    fn id(&self) -> Option<String> {
+        match self {
+            GridCell::Point { id, .. } => Some(id.clone()),
+            _ => None
+        }
+    }
+    fn get_territory_id(&self) -> Option<String> {
+        match self {
+            GridCell::Point { id, .. } => Some(id.clone()),
+            GridCell::TerritoryOfPoint { point_id, .. } => Some(point_id.clone()),
+            _ => None,
+        }
+    }
+    fn row(&self) -> Option<usize> {
+        match self {
+            GridCell::Point { row, .. } => Some(*row),
+            _ => None
+        }
+    }
+    fn col(&self) -> Option<usize> {
+        match self {
+            GridCell::Point { col, .. } => Some(*col),
+            _ => None
+        }
     }
 }
 
-impl FromStr for Point {
+impl FromStr for GridCell {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let x: usize = s.chars()
-            .take_while(|c| c.to_string() != ",")
-            .collect::<String>()
-            .parse()
-            .unwrap();
-        let y: usize = s.chars()
+        let row: usize = s.chars()
             .skip_while(|c| !c.is_whitespace())
             .skip(1)
             .collect::<String>()
             .parse()
             .unwrap();
+        let col: usize = s.chars()
+            .take_while(|c| c.to_string() != ",")
+            .collect::<String>()
+            .parse()
+            .unwrap();
 
-        Ok(Point::self_owned(x - 1, y - 1))
+        Ok(GridCell::point(row - 1, col - 1))
     }
 }
 
 pub fn get_largest_area() -> i32 {
-    let points = get_points();
-    let grid = make_grid(&points);
-    let ignore_ids = infinite_area_points_ids(&grid);
-
-    println!("{:?}", ignore_ids);
-
-    let areas = grid.iter()
+    let grid = build_grid();
+    let points_with_infinite_territory = points_with_infinite_territory(&grid);
+    let (point_id, &area_of_territory) = grid.iter()
         .flatten()
-        .filter(move |p| ignore_ids.contains(&p.id))
+        .filter(move |cell| {
+            let territory_id = match cell {
+                GridCell::Point { id, .. } => id,
+                GridCell::TerritoryOfPoint { point_id } => point_id,
+                _ => return false
+            };
+            !points_with_infinite_territory.contains(territory_id)
+        })
+        .filter(|cell| match cell {
+            GridCell::Point { .. } | GridCell::TerritoryOfPoint { .. } => true,
+            GridCell::Border => false,
+            _ => panic!("Critical logic error"),
+        })
         .fold(HashMap::new(),
               |mut area_by_point: HashMap<String, i32>, point| {
-                  let entry = area_by_point.entry(point.clone().id).or_default();
-                  *entry += 1;
+                  if let Some(id_of_territory) = point.clone().get_territory_id() {
+                      let entry = area_by_point.entry(id_of_territory).or_default();
+                      *entry += 1;
+                  }
                   area_by_point
-              });
+              })
+        .iter()
+        .max_by_key(|&(_, area_of_territory)| area_of_territory)
+        .expect("No territories found.");
 
-
-    println!("{:?}", areas);
-    1
+    area_of_territory
 }
 
-fn get_points() -> Vec<Point> {
-    BufReader::new(File::open(COORDS_FILE).unwrap())
+fn build_grid() -> Grid {
+    let points: Vec<GridCell> = BufReader::new(File::open(COORDS_FILE).unwrap())
         .lines()
-        .map(|l| Point::from_str(&l.unwrap()).unwrap())
-        .collect()
+        .map(|l| GridCell::from_str(&l.unwrap()).unwrap())
+        .collect();
+
+    let min_row = points.iter().min_by_key(|p| p.row().unwrap()).unwrap().row().unwrap();
+    let min_col = points.iter().min_by_key(|p| p.col().unwrap()).unwrap().col().unwrap();
+    let max_row = points.iter().max_by_key(|p| p.row().unwrap()).unwrap().row().unwrap() + 1;
+    let max_col = points.iter().max_by_key(|p| p.col().unwrap()).unwrap().col().unwrap() + 1;
+
+    let height = max_row - min_row;
+    let width = max_col - min_col;
+
+    // All points will be translated left/up by min_x/min_y
+    let points: Vec<GridCell> = points.iter()
+        .map(|p| GridCell::point(p.row().unwrap() - min_row, p.col().unwrap() - min_col))
+        .collect();
+
+    fill_grid_with(&points, height, width)
 }
 
 /// Construct a Grid, which surrounds all of the points (bounding box)
-fn make_grid(points: &Vec<Point>) -> Grid {
-    let min_x = points.iter().min_by_key(|p| p.x).unwrap().x;
-    let min_y = points.iter().min_by_key(|p| p.y).unwrap().y;
-    let max_x = points.iter().max_by_key(|p| p.x).unwrap().x + 1;
-    let max_y = points.iter().max_by_key(|p| p.y).unwrap().y + 1;
-
-    println!("minx {}, miny {}, maxx {}, maxy {}", min_x, min_y, max_x, max_y);
-
-    // All points will be translated left/up by min_x/min_y
-    let height = max_y - min_y;
-    let width = max_x - min_x;
-
-    println!("height {}, width {}", height, width);
-
+fn fill_grid_with(points: &Vec<GridCell>, height: usize, width: usize) -> Grid {
     let mut grid = points.iter()
-        .fold(vec![vec![Point::new(); width]; height],
-              |mut grid, point| {
-                  let trans_x = point.x - min_x;
-                  let trans_y = point.y - min_y;
-                  grid[trans_y][trans_x] = Point::self_owned(trans_x, trans_y);
+        .fold(vec![vec![GridCell::Empty; width]; height],
+              |mut grid, p| {
+                  grid[p.row().unwrap()][p.col().unwrap()] = p.clone();
                   grid
               });
 
     populate_closest_points(&points, &mut grid);
-
-    println!("{:?}", grid);
     grid
 }
 
-fn populate_closest_points(points: &Vec<Point>, grid: &mut Grid) {
-    for y in 0..grid.len() {
-        let row = &grid[y];
-        for x in 0..row.len() {
+fn populate_closest_points(points: &Vec<GridCell>, grid: &mut Grid) {
+    for row_index in 0..grid.len() {
+        let row = &grid[row_index];
+        for col_index in 0..row.len() {
             // For this cell, find the Manhattan distances to all points
             let mut distances = points.iter()
                 .map(|p| {
-                    /*
-                    ..........
-                    .A........
-                    ..........
-                    .......C..
-                    x..D......
-                    .....E....
-                    .B........
-                    ..........
-                    ..........
-                    ........F.
-                    */
-                    let manhattan = (p.x as i32 - x as i32).abs() + (p.y as i32 - y as i32).abs();
-//                    println!(
-//                        "({}, {}) has Manhattan distance {} to point at ({}, {})",
-//                        x, y, manhattan, p.x, p.y
-//                    );
+                    let manhattan = (p.col().unwrap() as i32 - col_index as i32).abs() +
+                        (p.row().unwrap() as i32 - row_index as i32).abs();
+                    (p.id().unwrap(), manhattan)
+                });
 
-                    (p, manhattan)
-                })
-                .filter(|(_, dist)| *dist > 0);
-
+            let mut cell_territory = GridCell::in_territory_of(distances.clone().next().unwrap().0);
             let mut min_distance = distances.clone().next().unwrap().1;
             let mut min_distance_count = 0;
-            for (point, manhattan) in distances.clone() {
+
+            for (point_id, manhattan) in distances.clone() {
                 if manhattan < min_distance {
                     min_distance = manhattan;
+                    cell_territory = GridCell::in_territory_of(point_id);
                     min_distance_count = 1;
                 } else if manhattan == min_distance {
                     min_distance_count += 1;
                 }
             }
-
             /*
                     Aaaa.ccc
                     aaddeccc
@@ -159,24 +183,20 @@ fn populate_closest_points(points: &Vec<Point>, grid: &mut Grid) {
                     bb.ffffF
             */
 
-            if grid[y][x].closest_id.is_none() {
-                if min_distance_count > 1 {
-                    println!("Setting grid[{}][{}] = None", y, x);
-                    grid[y][x].closest_id = None;
-                } else {
-                    let (closest, dist) = distances
-                        .min_by_key(|&(_, manhattan_distance)| manhattan_distance)
-                        .unwrap();
-                    println!("Setting grid[{}][{}] closest point at grid[{}][{}]", y, x, closest.y, closest.x);
-                    grid[y][x].closest_id = Some(closest.clone().id);
-                }
+            if min_distance_count > 1 {
+                // If we're tied closest to multiple points, we're on a border cell
+                grid[row_index][col_index] = GridCell::Border;
+            } else {
+                // Otherwise this cell is within the territory of a point
+                grid[row_index][col_index] = cell_territory;
             }
         }
     }
 }
 
-fn infinite_area_points_ids(grid: &Grid) -> HashSet<String> {
-    // Move around the outskirts of the grid, noting any points
+fn points_with_infinite_territory(grid: &Grid) -> HashSet<String> {
+    // Move around the outskirts of the grid, noting the closest points
+    // The closest recorded points of each of these cells have an infinite area and should be ignored.
     let num_rows = grid.len();
     let num_cols = grid[0].len();
 
@@ -185,12 +205,25 @@ fn infinite_area_points_ids(grid: &Grid) -> HashSet<String> {
         .flat_map(move |(row_idx, row)| {
             row.iter()
                 .enumerate()
-                .filter(move |(col_idx, point)| {
+                .filter(move |(col_idx, cell)| {
                     let is_horizontal_edge = row_idx == 0usize || row_idx == num_rows - 1;
                     let is_vertical_edge = *col_idx == 0usize || *col_idx == num_cols - 1;
                     is_horizontal_edge || is_vertical_edge
                 })
-                .map(|(col_idx, p)| p.clone().id)
+                .filter(|(col_idx, cell)| {
+                    match cell {
+                        GridCell::Empty => panic!("Empty cell after populating grid."),
+                        GridCell::Border => false,
+                        _ => true,
+                    }
+                })
+                .map(move |(col_idx, cell)| {
+                    match cell {
+                        GridCell::TerritoryOfPoint { point_id } => point_id.to_string(),
+                        GridCell::Point { id, .. } => id.to_string(),
+                        _ => panic!("Critical logic error.")
+                    }
+                })
         })
         .collect()
 }
