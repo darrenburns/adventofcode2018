c26b910527188233da07e410f09ee3716c77123e
Darren Burns <darren.burns@fanduel.com>
2018-12-21 03:29:36 +0000

    day 12 part 2

--- src/twelve.rs
+++ src/twelve.rs
@@ -1,13 +1,14 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
-use std::io::BufRead;
-use std::io::BufReader;
+use std::io::{BufRead, BufReader};
 
 const INITIAL_STATE: &'static str = "###......#.#........##.###.####......#..#####.####..#.###..#.###.#..#..#.#..#..#.##...#..##......#.#";
 const NUM_GENERATIONS: i32 = 20;
+const FIFTY_BILLION: i64 = 50_000_000_000;
 
-pub fn get_living_plant_numbers_summed() -> i32 {
-    let lines = BufReader::new(File::open("src/files/twelve/notes.txt").unwrap()).lines().map(|l| l.unwrap());
+pub fn get_living_plant_numbers_summed() -> i64 {
+    let lines = BufReader::new(File::open("src/files/twelve/notes.txt").unwrap()).lines()
+        .map(|l| l.unwrap());
 
     let mut notes_map = lines.fold(HashMap::new(), |mut map, line| {
         let (key, value) = scan_fmt!(&line, "{} => {}", String, String);
@@ -16,8 +17,10 @@ pub fn get_living_plant_numbers_summed() -> i32 {
     });
 
 
+    let mut seen_plants = HashSet::new();
     let mut current_generation = INITIAL_STATE.to_string();
-    for generation in 1..NUM_GENERATIONS + 1 {
+    let mut generation = 0;
+    loop {
         let mut next_generation = "".to_string();
         let padded_current = "....".to_string() + &current_generation + "....";
         for plant in 2..padded_current.len() - 2 {
@@ -25,20 +28,44 @@ pub fn get_living_plant_numbers_summed() -> i32 {
             next_generation.push_str(notes_map.get(&neighbours).unwrap());
         }
 
-        current_generation = next_generation;
-        println!("{:2}: {}", generation, current_generation);
-    }
+        let just_plants = just_plants(current_generation.clone());
+        if seen_plants.contains(&just_plants) {
+            // We've reached the point of convergence
+            let num_remaining_generations = FIFTY_BILLION - generation;
+            let current_gen_sum = sum_plants(-2 * generation, current_generation.clone());
+            let next_gen_sum = sum_plants(-2 * (generation + 1), next_generation.clone());
+            let increase_each_gen = next_gen_sum - current_gen_sum;
+            let sum_for_remaining_gens = num_remaining_generations * increase_each_gen;
+            let final_sum = current_gen_sum + sum_for_remaining_gens;
+            println!("gen [{}] current_sum = {} increase_each_gen = {}, num_remaining_gens = {}", generation, current_gen_sum, increase_each_gen, num_remaining_generations);
+            return final_sum;
+        } else {
+            seen_plants.insert(just_plants);
+        }
 
-    let lowest_pot_num = -2 * NUM_GENERATIONS;
-    sum_plants(lowest_pot_num, current_generation)
+        current_generation = next_generation;
+        generation += 1;
+    }
 }
 
-fn sum_plants(lowest_pot_num: i32, generation: String) -> i32 {
-    (lowest_pot_num..generation.len() as i32)
+fn sum_plants(lowest_pot_num: i64, generation: String) -> i64 {
+    (lowest_pot_num..generation.len() as i64)
         .zip(generation.chars())
         .filter(|&(pot_num, ch)| ch == '#')
         .map(|(pot_num, _)| {
             pot_num
         })
-        .sum()
+        .sum::<i64>()
 }
+
+fn just_plants(generation: String) -> String {
+    generation.replace(".", " ").trim().replace(" ", ".")
+}
+
+// Eventually the pattern will converge, and the plants will shift along
+// by a single pot each generation. As a result, the sum will increase by
+// a consistent amount each time (in my case 42 each time).
+
+// From the point where we reach this convergence, find how many generations we are
+// from 50 billion (50 billion - generation), and multiply this by 42, then add
+// this on to the existing sum.
